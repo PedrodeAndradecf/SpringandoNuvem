b8a0b0933e78db1cd97bd3e174883d52d82edc0a
PedrodeAndradecf <pedro.cfandrade@gmail.com>
2025-05-24 17:01:41 -0300

    CORS corrigido p 3000

--- src/main/java/Spring/SpringandoNuvem/controller/TaskController.java
+++ src/main/java/Spring/SpringandoNuvem/controller/TaskController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/task")
 public class TaskController {
